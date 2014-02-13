4d009634ecc3bee416417bba80c9b4ad1cb9b196
Josue E Basurto <josuebasurto@gmail.com>
2014-02-13 15:01:57 -0800

    Se agrega funcionalidad de mostrar el user

--- gplussigninlogic.js
+++ gplussigninlogic.js
@@ -3,6 +3,35 @@
 	by Josue Basurto
 */
 
+//Login lyfecycle
+function signinCallback(authResult) {
+    if (authResult.status.google_logged_in) {
+
+        //Logged in!
+        console.log('User Logged in');
+
+        gapi.client.load('plus', 'v1', function () {
+            var request = gapi.client.plus.people.get({ 'userId': 'me' });
+            request.execute(function (resp) {
+			 fillInfo(resp);
+            });
+        });
+
+    } else {
+	   //Didnt log in
+        var state = 'Sign-in state: ' + authResult['error'];
+        console.log(state);
+    }
+}
+
+//Fill Info for user
+function fillInfo(resp){
+	console.log('Hola:' + resp.displayName);
+	$("#signinButton").fadeOut("fast", function () {});
+	$("#badgeImage").attr('src',resp.image.url)	
+	$("#badgeContent").fadeIn("slow", function () {});
+}
+
 //Google Code for Google Plus Sign In
 (function () {
     var po = document.createElement('script');
