f505b0fb7a651946f274f37da8e0a22ea47e0b29
Josue E Basurto <josuebasurto@gmail.com>
2014-02-13 15:01:00 -0800

    Se agrega codigo default

--- gplussigninlogic.js
+++ gplussigninlogic.js
@@ -2,3 +2,13 @@
 	Google Plus Sign In Logic
 	by Josue Basurto
 */
+
+//Google Code for Google Plus Sign In
+(function () {
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://apis.google.com/js/client:plusone.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+})();
